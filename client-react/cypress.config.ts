91dcd0bced79dcb6bf92adc7aa8a574884cf6fd8
Ely Lucas <ely@meta-tek.net>
2023-05-31 16:07:23 -0600

    updating projectId

--- client-react/cypress.config.ts
+++ client-react/cypress.config.ts
@@ -3,12 +3,12 @@ import { createHero, deleteHero } from './cypress/support/data';
 import fs from 'fs';
 
 export default defineConfig({
-  projectId: '2grnur',
+  projectId: 'x2youg',
   e2e: {
     video: true,
     baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {
-      on('task', {  
+      on('task', {
         createHero,
         deleteHero,
       });
